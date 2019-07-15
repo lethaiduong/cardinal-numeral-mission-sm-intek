a11a2757dfa4419ea528971014d0b3878167edba
Nguyen Tuan Dat <tuandat99kz@gmail.com>
2019-07-15 16:49:54 +0700

    Done waypoint 3 and made code cleaner

--- src/cardinal_numeral.py
+++ src/cardinal_numeral.py
@@ -1,3 +1,26 @@
+# Defined numbers
+NUMERAL_ZERO    =   "không"
+NUMERAL_ONE1    =   "một"
+NUMERAL_ONE2    =   "mốt"
+NUMERAL_TWO     =   "hai"
+NUMERAL_THREE   =   "ba"
+NUMERAL_FOUR    =   "bốn"
+NUMERAL_FIVE1   =   "năm"
+NUMERAL_FIVE2   =   "lăm"
+NUMERAL_SIX     =   "sáu"
+NUMERAL_SEVEN   =   "bảy"
+NUMERAL_EIGHT   =   "tám"
+NUMERAL_NINE    =   "chín"
+NUMERAL_TEN1    =   "mười"
+NUMERAL_TEN2    =   "mươi"
+ONES1           =   "linh"
+ONES2           =   "lẻ"
+HUNDREDS        =   "trăm"
+THOUSANDS1      =   "nghìn"
+THOUSANDS2      =   "ngàn"
+MILLIONS        =   "triệu"
+BILLIONS        =   "tỷ"
+
 # User defined custom exception
 class Overflow(Exception):
     """Integer greater than 999,999,999,999"""
@@ -5,86 +28,90 @@ class Overflow(Exception):
 
 
 # Return list cardinal numeral words from number of units
-def units(n):
+def convert_units(n):
     res = []
     switcher = {
-            0: "không",
-            1: "một",
-            2: "hai",
-            3: "ba",
-            4: "bốn",
-            5: "năm",
-            6: "sáu",
-            7: "bảy",
-            8: "tám",
-            9: "chín"
+            0: NUMERAL_ZERO,
+            1: NUMERAL_ONE1,
+            2: NUMERAL_TWO,
+            3: NUMERAL_THREE,
+            4: NUMERAL_FOUR,
+            5: NUMERAL_FIVE1,
+            6: NUMERAL_SIX,
+            7: NUMERAL_SEVEN,
+            8: NUMERAL_EIGHT,
+            9: NUMERAL_NINE
     }
     res.append(switcher[n])
     return res
 
 
-# Return list cardinal numeral words from number of dozens
-def dozens(n):
+# Return list cardinal numeral words from number of tens
+def convert_tens(n):
     res = []
 
-    # Convert digit of dozens
+    # Convert digit of tens
     if n // 10 == 1:
-        res.append("mười")
+        res.append(NUMERAL_TEN1)
     else:
         switcher = {
-                2: "hai",
-                3: "ba",
-                4: "bốn",
-                5: "năm",
-                6: "sáu",
-                7: "bảy",
-                8: "tám",
-                9: "chín"
+                2: NUMERAL_TWO,
+                3: NUMERAL_THREE,
+                4: NUMERAL_FOUR,
+                5: NUMERAL_FIVE1,
+                6: NUMERAL_SIX,
+                7: NUMERAL_SEVEN,
+                8: NUMERAL_EIGHT,
+                9: NUMERAL_NINE
         }
         res.append(switcher[n // 10])
-        res.append("mươi")
+        res.append(NUMERAL_TEN2)
 
     # Convert digit of units
     if n % 10 != 0:
         if n % 10 != 1:
             switcher = {
-                    2: "hai",
-                    3: "ba",
-                    4: "bốn",
-                    5: "lăm",
-                    6: "sáu",
-                    7: "bảy",
-                    8: "tám",
-                    9: "chín"
+                    2: NUMERAL_TWO,
+                    3: NUMERAL_THREE,
+                    4: NUMERAL_FOUR,
+                    5: NUMERAL_FIVE2,
+                    6: NUMERAL_SIX,
+                    7: NUMERAL_SEVEN,
+                    8: NUMERAL_EIGHT,
+                    9: NUMERAL_NINE
             }
             res.append(switcher[n % 10])
         else:
             if n // 10 == 1:
-                res.append("một")
+                res.append(NUMERAL_ONE1)
             else:
-                res.append("mốt")
+                res.append(NUMERAL_ONE2)
     return res
 
 
 # Return list cardinal numeral words from number of hundreds
-def hundreds(n):
+def convert_hundreds(n):
     res = []
-    res = res + units(n // 100)
-    res.append("trăm")
+    res = res + convert_units(n // 100)
+    res.append(HUNDREDS)
 
     if n % 100 != 0:
         if n % 100 >= 10:
-            res = res + dozens(n % 100)
+            res = res + convert_tens(n % 100)
         else:
-            res.append("linh")
-            res = res + units(n % 100)
+            res.append(ONES1)
+            res = res + convert_units(n % 100)
     return res
 
 
 # Waypoint 1
-def integer_to_vietnamese_numeral(n):
-    res = []
+def integer_to_vietnamese_numeral(n, activate_tts = False):
+    if (type(activate_tts) is not bool) and (type(activate_tts) is not None):
+        raise TypeError('Argument "activate_tts" is not a boolean')
+    if activate_tts is None:
+        activate_tts = False
 
+    res = []
     # Catch exceptions
     if type(n) is not int:
         raise TypeError("Not an integer")
@@ -98,23 +125,33 @@ def integer_to_vietnamese_numeral(n):
         if n // i > 0:
             if len(res) == 0:
                 if n // i >= 100:
-                    res = res + hundreds(n // i)
+                    res = res + convert_hundreds(n // i)
                 elif n // i >= 10:
-                    res = res + dozens(n // i)
+                    res = res + convert_tens(n // i)
                 else:
-                    res = res + units(n // i)
+                    res = res + convert_units(n // i)
             else:
-                res = res + hundreds(n // i)
+                res = res + convert_hundreds(n // i)
 
             if i == 1000000000:
-                res.append("tỷ")
+                res.append(BILLIONS)
             elif i == 1000000:
-                res.append("triệu")
+                res.append(MILLIONS)
             elif i == 1000:
-                res.append("nghìn")
+                res.append(THOUSANDS1)
 
             n = n % i
 
-    return res
+    if not activate_tts:
+        return res
+    else:
+        import pygame
+        pygame.init()
+        for word in res:
+            sound = pygame.mixer.Sound("../sounds/vie/north/" + word + ".ogg")
+            sound.play(maxtime = 2200)
+            print("../sounds/vie/north/" + word + ".ogg")
+            pygame.time.delay(400)
+        return res
 
 ##################################################################
--- src/test.py
+++ src/test.py
@@ -1,2 +1,2 @@
 import cardinal_numeral as cn
-print(" ".join(cn.integer_to_vietnamese_numeral(423232323232)))
+print(" ".join(cn.integer_to_vietnamese_numeral(906030201, True)))
